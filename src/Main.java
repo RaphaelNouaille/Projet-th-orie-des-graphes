347db74be31303977e254d027ed35d994d6e4d1f
RaphaelNouaille <raphael.nouailledegorce@edu.ece.fr>
2025-11-18 16:10:30 +0100

    ?

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Salut les mecs!");
+        System.out.println("Ou es tu Dylane ?");
+
 
     }
 }
