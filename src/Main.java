44f98332353e388a849a4f4e5c7198efc0379756
RaphaelNouaille <raphael.nouailledegorce@edu.ece.fr>
2025-11-18 16:17:28 +0100

    Florin

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Salut les mecs!");
-        System.out.println("Ou es tu Dylane ?");
+        System.out.println("Ou es tu Florin ?");
 
 
     }
